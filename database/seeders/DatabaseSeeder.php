6e24026dfa6494a3ee8bd90844a3c5f6bb50d2c0
Gabriel Vitor Silva Barbosa <gabriel.vitorsb323@gmail.com>
2022-07-05 08:19:45 -0300

    Adicionando seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Editora;
+use App\Models\Emprestimo;
+use App\Models\Livro;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->count(3)->create();
+        Editora::factory()->count(3)->create();
+        Livro::factory()->count(3)->create();
+        Emprestimo::factory()->create();
     }
 }
